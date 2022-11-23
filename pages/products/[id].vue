49fb50e389fd20ca5672eaeef860175506e2a201
Muna Nadhifa Ridwan <mnadhifa@prixa.ai>
2022-11-23 14:21:57 +0700

    update

--- pages/products/[id].vue
+++ pages/products/[id].vue
@@ -1,6 +1,21 @@
-
-
 <template>
+  <div>
+    <header class="border-b border-gray-500 pb-2">
+      <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+        <div class="relative">
+          <div>
+            <div class="items-center pt-2">
+              <NuxtLink to="/">
+                <a href="#" class="-m-2 block p-2 font-medium text-gray-900"
+                  >Home</a
+                >
+              </NuxtLink>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+  </div>
   <div class="bg-white">
     <div
       class="mx-auto max-w-2xl py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8"
